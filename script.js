50a15042bc031195e055c8f18d227942b6a262f8
bigptlg <148574025+bigptlg@users.noreply.github.com>
2024-05-02 08:56:59 +0100

    Script menu
    Agora é possivel fechar o menu clicando fora em qualquer lugar que não faça parte do menu

--- script.js
+++ script.js
@@ -27,3 +27,61 @@ $(document).ready(function(){
   $("#daMode").click(darkMode);
 
 });
+$(document).ready(function(){
+    
+  $("#menuIcon").click(function(){
+    $("header").toggleClass("menu");
+  });
+    
+    function darkMode(){
+
+    $("body").toggleClass("dark");
+
+    if ($("body").hasClass("dark")) {
+      $("#daMode").removeClass("fa-solid fa-moon");
+      $("#daMode").addClass("fa-solid fa-sun");
+      localStorage.setItem("darkModeAtivado", "true");
+    } else {
+      $("#daMode").removeClass("fa-solid fa-sun");
+      $("#daMode").addClass("fa-solid fa-moon");
+      localStorage.setItem("darkModeAtivado", "false");
+    }
+
+  };
+
+  if(localStorage.getItem("darkModeAtivado") === "true"){
+    darkMode();
+  }
+  
+  $("#daMode").click(darkMode);
+
+});
+$(document).ready(function(){
+    
+  $("#menuIcon").click(function(){
+    $("header").toggleClass("menu");
+  });
+    
+    function darkMode(){
+
+    $("body").toggleClass("dark");
+
+    if ($("body").hasClass("dark")) {
+      $("#daMode").removeClass("fa-solid fa-moon");
+      $("#daMode").addClass("fa-solid fa-sun");
+      localStorage.setItem("darkModeAtivado", "true");
+    } else {
+      $("#daMode").removeClass("fa-solid fa-sun");
+      $("#daMode").addClass("fa-solid fa-moon");
+      localStorage.setItem("darkModeAtivado", "false");
+    }
+
+  };
+
+  if(localStorage.getItem("darkModeAtivado") === "true"){
+    darkMode();
+  }
+  
+  $("#daMode").click(darkMode);
+
+});
